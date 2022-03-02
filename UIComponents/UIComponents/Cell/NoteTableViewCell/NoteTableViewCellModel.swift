3d7efe884b6b34f910bf459e86d6086cbb68cdbe
Mehmet Salih Celik <mehmetsalih-celik@hotmail.com>
2022-03-02 11:12:34 +0300

    create note cell & model

---
+++ UIComponents/UIComponents/Cell/NoteTableViewCell/NoteTableViewCellModel.swift
@@ -0,0 +1,37 @@
+//
+//  NoteTableViewCellModel.swift
+//  UIComponents
+//
+//  Created by Mehmet Salih ÇELİK on 24.02.2022.
+//
+
+import Foundation
+
+public protocol NoteTableViewCellDataSource: AnyObject {
+    var title: String { get set }
+    var description: String { get set }
+    var id: Int { get set }
+}
+
+public protocol NoteTableViewCellEventSource: AnyObject {
+    
+}
+
+public protocol NoteTableViewCellProtocol: NoteTableViewCellDataSource, NoteTableViewCellEventSource {
+    
+}
+
+public final class NoteTableViewCellModel: NoteTableViewCellProtocol {
+    
+    public var title: String
+    public var description: String
+    public var id: Int
+    
+    public init(title: String, description: String, id: Int) {
+        self.title = title
+        self.description = description
+        self.id = id
+    }
+    
+    
+}
