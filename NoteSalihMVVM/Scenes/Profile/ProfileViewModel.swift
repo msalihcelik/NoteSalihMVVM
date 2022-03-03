14f44b4a3d1360c962ba9b463537f73ef8c81ed2
Mehmet Salih Celik <mehmetsalih-celik@hotmail.com>
2022-03-03 12:52:35 +0300

    add network methods to viewmodel
    request & response

--- NoteSalihMVVM/Scenes/Profile/ProfileViewModel.swift
+++ NoteSalihMVVM/Scenes/Profile/ProfileViewModel.swift
@@ -25,14 +25,52 @@ final class ProfileViewModel: BaseViewModel<ProfileRouter>, ProfileViewProtocol 
     
     var user: User = User(fullName: "", email: "", id: 0)
     var reloadUser: VoidClosure?
+    
+    var getFullName: String {
+        return user.fullName ?? ""
+    }
+    
+    var getEmail: String {
+        return user.email ?? ""
+    }
+}
 
+// MARK: - Network
+extension ProfileViewModel {
+    
     func getUser() {
-        
+        showLoading?()
+        dataProvider.request(for: GetUserRequest()) { [weak self] result in
+            guard let self = self else { return }
+            self.hideLoading?()
+            switch result {
+            case .success(let response):
+                guard let data = response.data else { return }
+                self.user = data
+                self.reloadUser?()
+            case .failure(let error):
+                self.showWarningToast?(error.localizedDescription)
+            }
+        }
     }
     
     func updateUser(userName: String, email: String) {
-        
+        showLoading?()
+        dataProvider.request(for: UpdateUserRequest(fullName: userName, email: email)) { [weak self] result in
+            guard let self = self else { return }
+            self.hideLoading?()
+            switch result {
+            case .success(let response):
+                self.showSuccessToast?(L10n.Profile.success)
+            case .failure(let error):
+                self.showWarningToast?(error.localizedDescription)
+            }
+        }
     }
+}
+
+// MARK: - Actions
+extension ProfileViewModel {
     
     func leftIconTapped() {
         router.close()
