948ef5f1261976a5b04cdecfa130501d39fefff4
Mehmet Salih Celik <mehmetsalih-celik@hotmail.com>
2022-03-03 17:27:13 +0300

    fill network & action methods

--- NoteSalihMVVM/Scenes/ChangePassword/ChangePasswordViewModel.swift
+++ NoteSalihMVVM/Scenes/ChangePassword/ChangePasswordViewModel.swift
@@ -13,4 +13,36 @@ protocol ChangePasswordViewProtocol: ChangePasswordViewDataSource, ChangePasswor
 
 final class ChangePasswordViewModel: BaseViewModel<ChangePasswordRouter>, ChangePasswordViewProtocol {
     
+    func sameAllError() {
+        self.showWarningToast?(L10n.ChangePassword.sameOldPassword)
+    }
+    
+    func differentPasswordError() {
+        self.showWarningToast?(L10n.ChangePassword.differentNewPassword)
+    }
+}
+
+// MARK: - Network
+extension ChangePasswordViewModel {
+    
+    func changePassword(password: String, newPassword: String, retypeNewPassword: String) {
+        dataProvider.request(for: ChangePasswordRequest(password: password,
+                                                        newPassword: newPassword,
+                                                        newPasswordConfirmation: retypeNewPassword)) { [weak self] result in
+            switch result {
+            case .success:
+                ToastPresenter.showSuccessToast(text: L10n.ChangePassword.success)
+            case .failure(let error):
+                ToastPresenter.showWarningToast(text: error.localizedDescription)
+            }
+        }
+    }
+}
+
+// MARK: - Action
+extension ChangePasswordViewModel {
+    
+    func leftIconTapped() {
+        router.close()
+    }
 }
