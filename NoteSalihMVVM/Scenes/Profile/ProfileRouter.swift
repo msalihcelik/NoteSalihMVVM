30a99e3b7c0c6bc2d26620f2eb2141ae7a272bfd
Mehmet Salih Celik <mehmetsalih-celik@hotmail.com>
2022-03-03 11:41:35 +0300

    add root route

--- NoteSalihMVVM/Scenes/Profile/ProfileRouter.swift
+++ NoteSalihMVVM/Scenes/Profile/ProfileRouter.swift
@@ -5,4 +5,6 @@
 //  Created by Mehmet Salih ÇELİK on 3.03.2022.
 //
 
-final class ProfileRouter: Router {}
+final class ProfileRouter: Router, ProfileRouter.Routes {
+    typealias Routes = PopToRootRoute
+}
