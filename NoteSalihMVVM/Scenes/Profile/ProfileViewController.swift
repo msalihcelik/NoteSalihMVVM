a224c4ed4fcdca59d690c5c5bc71960494bd7fe3
Mehmet Salih Celik <mehmetsalih-celik@hotmail.com>
2022-03-03 11:22:44 +0300

    create ui & add base methods

--- NoteSalihMVVM/Scenes/Profile/ProfileViewController.swift
+++ NoteSalihMVVM/Scenes/Profile/ProfileViewController.swift
@@ -5,12 +5,122 @@
 //  Created by Mehmet Salih ÇELİK on 3.03.2022.
 //
 
-import UIKit
-
 final class ProfileViewController: BaseViewController<ProfileViewModel> {
+    
+    private let stackView = UIStackViewBuilder()
+        .spacing(15)
+        .axis(.vertical)
+        .build()
+    private let fullNameTextField = AuthTextField()
+    private let emailTextField = AuthTextField()
+    private let saveButton = AuthButton()
+    private let changePasswordButton = UIButtonBuilder()
+        .build()
+    private let signOutButton = UIButtonBuilder()
+        .build()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        addSubViews()
+        configureContents()
+        setLocalize()
+        subscribeViewModel()
+        getUser()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationItem.setHidesBackButton(true, animated: true)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationItem.setHidesBackButton(false, animated: true)
+    }
+}
+
+// MARK: - UILayout
+extension ProfileViewController {
+    
+    private func addSubViews() {
+        view.addSubview(stackView)
+        stackView.edgesToSuperview(excluding: .bottom, insets: .init(top: 25, left: 25, bottom: 25, right: 25), usingSafeArea: true)
+        stackView.addArrangedSubview(fullNameTextField)
+        stackView.addArrangedSubview(emailTextField)
+        stackView.addArrangedSubview(saveButton)
+        stackView.addArrangedSubview(changePasswordButton)
+        stackView.addArrangedSubview(signOutButton)
+    }
+}
+
+// MARK: - Configure & SetLocalize
+extension ProfileViewController {
+    
+    private func configureContents() {
+        configureNavigation()
+        configureSaveButton()
+        configureChangePasswordButton()
+        configureSignOutButton()
+    }
+    
+    private func configureNavigation() {
+        let leftIcon = UIBarButtonItem(image: .icHamburger, style: .plain, target: self, action: #selector(leftIconTapped))
+        navigationItem.leftBarButtonItems = [leftIcon]
+    }
+    
+    private func configureSaveButton() {
+        saveButton.addTarget(self, action: #selector(saveButtonTapped), for: .touchUpInside)
+    }
+    
+    private func configureChangePasswordButton() {
+        changePasswordButton.titleLabel?.font = .font(.josefinSansSemiBold, size: 14)
+        changePasswordButton.addTarget(self, action: #selector(changePasswordTapped), for: .touchUpInside)
+    }
+    
+    private func configureSignOutButton() {
+        signOutButton.titleLabel?.font = .font(.josefinSansSemiBold, size: 14)
+        signOutButton.setTitleColor(.appRed, for: .normal)
+        signOutButton.addTarget(self, action: #selector(signOutTapped), for: .touchUpInside)
+    }
+    
+    private func setLocalize() {
+        title = L10n.Profile.profile
+        saveButton.setTitle(L10n.Profile.save, for: .normal)
+        changePasswordButton.setTitle(L10n.Profile.changePassword, for: .normal)
+        signOutButton.setTitle(L10n.Profile.signOut, for: .normal)
+    }
+}
+
+// MARK: - Actions
+extension ProfileViewController {
+
+    @objc
+    private func saveButtonTapped() {
+    
+    }
+    
+    @objc
+    private func changePasswordTapped() {
+        viewModel.changePasswordTapped()
+    }
+    
+    @objc
+    private func signOutTapped() {
+        viewModel.signOutTapped()
+    }
+    
+    @objc
+    private func leftIconTapped() {
+        viewModel.leftIconTapped()
+    }
+}
+
+// MARK: - SubscribeViewModel
+extension ProfileViewController {
+    
+    private func subscribeViewModel() {
+        viewModel.reloadUser = { [weak self] in
+            guard let self = self else { return }
+        }
+    }
 }
--- NoteSalihMVVM/Scenes/Profile/ProfileViewModel.swift
+++ NoteSalihMVVM/Scenes/Profile/ProfileViewModel.swift
@@ -5,14 +5,44 @@
 //  Created by Mehmet Salih ÇELİK on 3.03.2022.
 //
 
-import Foundation
+protocol ProfileViewDataSource {
+    
+}
 
-protocol ProfileViewDataSource {}
+protocol ProfileViewEventSource {
+    var reloadUser: VoidClosure? { get set }
+}
 
-protocol ProfileViewEventSource {}
-
-protocol ProfileViewProtocol: ProfileViewDataSource, ProfileViewEventSource {}
+protocol ProfileViewProtocol: ProfileViewDataSource, ProfileViewEventSource {
+    func getUser()
+    func updateUser(userName: String, email: String)
+    func leftIconTapped()
+    func changePasswordTapped()
+    func signOutTapped()
+}
 
 final class ProfileViewModel: BaseViewModel<ProfileRouter>, ProfileViewProtocol {
     
+    var user: User = User(fullName: "", email: "", id: 0)
+    var reloadUser: VoidClosure?
+
+    func getUser() {
+        
+    }
+    
+    func updateUser(userName: String, email: String) {
+        
+    }
+    
+    func leftIconTapped() {
+        
+    }
+    
+    func changePasswordTapped() {
+        
+    }
+    
+    func signOutTapped() {
+        
+    }
 }
